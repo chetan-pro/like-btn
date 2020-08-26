e75364597ba81bf744fb0ae911f9b3aa1355eb54
chetan sable <59227696+chetan-pro@users.noreply.github.com>
2020-08-26 20:14:15 +0530

    upload

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.material').click(function(){
+        $('.material').toggleClass("heart-active")
+        $('.text').toggleClass("heart-active")
+        $('.num').toggleClass("heart-active")
+        $('.heart').toggleClass("heart-active")
+    })
+})
